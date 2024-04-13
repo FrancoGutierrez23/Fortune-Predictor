7896f03ea3d6924dcd92f0f8d1870bb70f467a66
Franco Gutierrez <francogutierre523523@gmail.com>
2024-04-13 17:12:05 -0300

    Add the arrays of elements for love, job an numbers

---
+++ script.js
@@ -0,0 +1,11 @@
+const love = ['A new person will come', 'Be careful with companion', 'Maybe is time for adventures', 'Greats things coming soon'];
+const luckyNumbers = [];
+const job = ['Time for changes', 'You will have good news soon', 'You have to confront your boss', 'You deserve more'];
+
+let helperLuckyNumbers = () => {
+    for (let i = 1; i < 101; i++) {
+        luckyNumbers.push(i)
+    }
+};
+helperLuckyNumbers();
+
